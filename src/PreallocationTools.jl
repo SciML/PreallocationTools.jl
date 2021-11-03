a4241845bbc8409a9feee68a75e020f6075f190e
Thomas Vetter <80452158+thomvet@users.noreply.github.com>
2021-11-03 16:46:49 +0100

    support for nested duals; tests included

--- src/PreallocationTools.jl
+++ src/PreallocationTools.jl
@@ -19,7 +19,8 @@ end
 
 Builds a `DualCache` object that stores both a version of the cache for `u`
 and for the `Dual` version of `u`, allowing use of pre-cached vectors with
-forward-mode automatic differentiation. Supports nested AD.
+forward-mode automatic differentiation. Supports nested AD via keyword `levels`
+or specifying an array of chunk_sizes.
 
 """
 dualcache(u::AbstractArray, N::Int=ForwardDiff.pickchunksize(length(u)); levels::Int = 1) = DiffCache(u, size(u), N*ones(Int, levels))
