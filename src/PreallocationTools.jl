1b206c80adab0e4a0af332d31f493220fdc7e01d
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-10-13 09:08:36 +0200

    format

--- src/PreallocationTools.jl
+++ src/PreallocationTools.jl
@@ -60,7 +60,7 @@ function get_tmp(dc::DiffCache, u::AbstractArray{T}) where {T <: ForwardDiff.Dua
 end
 
 function get_tmp(dc::DiffCache, u::Union{Number, AbstractArray})
-    if promote_type(eltype(dc.du),eltype(u)) <: eltype(dc.du)
+    if promote_type(eltype(dc.du), eltype(u)) <: eltype(dc.du)
         dc.du
     else
         if length(dc.du) > length(dc.any_du)
