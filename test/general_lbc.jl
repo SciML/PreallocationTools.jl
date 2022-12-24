2c8c3780b82ca6663b003c1d4182c55afcc4aadd
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-12-24 18:16:14 -0500

    more formatting

--- test/general_lbc.jl
+++ test/general_lbc.jl
@@ -1,8 +1,11 @@
-using Random, OrdinaryDiffEq, LinearAlgebra, Optimization, OptimizationOptimJL, PreallocationTools
+using Random, OrdinaryDiffEq, LinearAlgebra, Optimization, OptimizationOptimJL,
+      PreallocationTools
 
 lbc = GeneralLazyBufferCache(function (p)
-    DifferentialEquations.init(ODEProblem(ode_fnc, y₀, (0.0, T), p), Tsit5(); saveat = t)
-end)
+                                 DifferentialEquations.init(ODEProblem(ode_fnc, y₀,
+                                                                       (0.0, T), p),
+                                                            Tsit5(); saveat = t)
+                             end)
 
 Random.seed!(2992999)
 λ, y₀, σ = -0.5, 15.0, 0.1
@@ -19,11 +22,12 @@ function loglik(θ, data, integrator)
     reinit!(integrator, u0)
     solve!(integrator, Tsit5())
     ε = yᵒ .- integrator.sol.u
-    ℓ = -0.5n * log(2π * σ^2) - 0.5 / σ^2 * sum(ε.^2)
+    ℓ = -0.5n * log(2π * σ^2) - 0.5 / σ^2 * sum(ε .^ 2)
 end
 θ₀ = [-1.0, 0.5, 19.73]
 negloglik = (θ, p) -> -loglik(θ, p, lbc[θ[1]])
 fnc = OptimizationFunction(negloglik, Optimization.AutoForwardDiff())
 ε = zeros(n)
-prob = OptimizationProblem(fnc, θ₀, (yᵒ, n, ε), lb=[-10.0, 1e-6, 0.5], ub=[10.0, 10.0, 25.0])
+prob = OptimizationProblem(fnc, θ₀, (yᵒ, n, ε), lb = [-10.0, 1e-6, 0.5],
+                           ub = [10.0, 10.0, 25.0])
 solve(prob, LBFGS())
