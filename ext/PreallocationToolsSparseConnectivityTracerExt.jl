1cc0d34dd8a87c3e27eacaeccf0127b026143080
Hendrik Ranocha <mail@ranocha.de>
2025-07-11 12:17:25 +0200

    support SparseConnectivityTracer

--- ext/PreallocationToolsSparseConnectivityTracerExt.jl
+++ ext/PreallocationToolsSparseConnectivityTracerExt.jl
@@ -1,46 +1,23 @@
 module PreallocationToolsSparseConnectivityTracerExt
 
-using PreallocationTools
-isdefined(Base, :get_extension) ? (import SparseConnectivityTracer) :
-(import ..SparseConnectivityTracer)
+using PreallocationTools: PreallocationTools, DiffCache, get_tmp
+using SparseConnectivityTracer: AbstractTracer, Dual
 
-function PreallocationTools.get_tmp(
-        dc::DiffCache, u::T) where {T <: SparseConnectivityTracer.Dual}
-    if isbitstype(T)
-        nelem = div(sizeof(T), sizeof(eltype(dc.dual_du))) * length(dc.du)
-        if nelem > length(dc.dual_du)
-            PreallocationTools.enlargediffcache!(dc, nelem)
-        end
-        PreallocationTools._restructure(dc.du, reinterpret(T, view(dc.dual_du, 1:nelem)))
-    else
-        PreallocationTools._restructure(dc.du, zeros(T, size(dc.du)))
-    end
+function PreallocationTools.get_tmp(dc::DiffCache, u::T) where {T <: Union{AbstractTracer, Dual}}
+    return get_tmp(dc, typeof(u))
 end
 
-function PreallocationTools.get_tmp(
-        dc::DiffCache, ::Type{T}) where {T <: SparseConnectivityTracer.Dual}
-    if isbitstype(T)
-        nelem = div(sizeof(T), sizeof(eltype(dc.dual_du))) * length(dc.du)
-        if nelem > length(dc.dual_du)
-            PreallocationTools.enlargediffcache!(dc, nelem)
-        end
-        PreallocationTools._restructure(dc.du, reinterpret(T, view(dc.dual_du, 1:nelem)))
-    else
-        PreallocationTools._restructure(dc.du, zeros(T, size(dc.du)))
-    end
+function PreallocationTools.get_tmp(dc::DiffCache, u::AbstractArray{<:T}) where {T <: Union{AbstractTracer, Dual}}
+    return get_tmp(dc, eltype(u))
 end
 
-function PreallocationTools.get_tmp(
-        dc::DiffCache, u::AbstractArray{T}) where {T <: SparseConnectivityTracer.Dual}
-    if isbitstype(T)
-        nelem = div(sizeof(T), sizeof(eltype(dc.dual_du))) * length(dc.du)
-        if nelem > length(dc.dual_du)
-            PreallocationTools.enlargediffcache!(dc, nelem)
-        end
-        PreallocationTools._restructure(dc.du, reinterpret(T, view(dc.dual_du, 1:nelem)))
-    else
-        PreallocationTools._restructure(dc.du, zeros(T, size(dc.du)))
-    end
+function PreallocationTools.get_tmp(dc::DiffCache, ::Type{T}) where {T <: Union{AbstractTracer, Dual}}
+    # We allocate memory here since we assume that sparsity connection happens only
+    # once (or maybe a few times). This simplifies the implementation and allows us
+    # to save memory in the long run since we do not need to store an additional
+    # cache for the sparsity detection that would be used only once but carried
+    # around forever.
+    return similar(dc.du, T)
 end
 
 end
--- src/PreallocationTools.jl
+++ src/PreallocationTools.jl
@@ -108,7 +108,7 @@ end
 Builds a `DiffCache` object that stores both a version of the cache for `u`
 and for the `Dual` version of `u`, allowing use of pre-cached vectors with
 forward-mode automatic differentiation. Supports nested AD via keyword `levels`
-or specifying an array of chunk_sizes.
+or specifying an array of chunk sizes.
 """
 function DiffCache(u::AbstractArray, N::Int = ForwardDiff.pickchunksize(length(u));
         levels::Int = 1)
